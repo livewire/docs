a779856bd07d912a40d9db7a91bed86a6390b06a
Caleb Porzio <calebporzio@gmail.com>
2020-02-25 11:33:02 -0500

    Build prod

--- listeners/GenerateSitemap.php
+++ listeners/GenerateSitemap.php
@@ -2,6 +2,7 @@
 
 namespace App\Listeners;
 
+use Illuminate\Support\Str;
 use samdark\sitemap\Sitemap;
 use TightenCo\Jigsaw\Jigsaw;
 
@@ -37,6 +38,6 @@ class GenerateSitemap
 
     public function isExcluded($path)
     {
-        return str_is($this->exclude, $path);
+        return Str::is($this->exclude, $path);
     }
 }
