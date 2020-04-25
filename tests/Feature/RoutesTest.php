4fdb7e4c52619bc185ed6c30b21ad3b4c0d1900f
Caleb Porzio <calebporzio@gmail.com>
2020-04-24 20:54:34 -0400

    wip

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -10,21 +10,21 @@ use Tests\TestCase;
 class RoutesTest extends TestCase
 {
     /** @test */
-    function hit_pages()
+    function hit_pages_and_make_sure_nothing_breaks()
     {
         // Home Page
         $this->get('/')->assertSuccessful();
+
+        // Screencasts
+        $this->followingRedirects()->get('/screencasts')->assertSuccessful();
+        Screencast::all()->each(function ($screencast) {
+            $this->get('/screencasts/'.$screencast->slug)->assertSuccessful();
+        });
 
         // Docs
         $this->followingRedirects()->get('/docs')->assertSuccessful();
         collect((new DocumentationPages(''))->all())->flatten()->each(function ($slug) {
             $this->get('/docs/'.$slug)->assertSuccessful();
-        });
-
-        // Screencasts
-        $this->followingRedirects()->get('/screencasts')->assertSuccessful();
-        Screencast::all()->each(function ($screencast) {
-            $this->get('/screencasts/'.$screencast->slug);
         });
 
         // Podcasts
