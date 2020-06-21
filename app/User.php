b2e57d6d62eca93b0e3c9b540c3b23325ac0362e
Caleb Porzio <calebporzio@gmail.com>
2020-06-21 10:36:06 -0400

    wip

--- app/User.php
+++ app/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable
             // Neerav Pandya
             'neeravp',
             'RicardoRamirezR',
+            'ngcammayo',
         ])) return true;
 
         return !! $this->sponsor;
