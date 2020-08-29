c4f4b8035cb630d6aa596f98b8d996fd731f9ee0
Dan Harrin <git@DanHarrin.com>
2020-08-30 00:33:52 +0100

    Remove old docs

--- resources/views/docs/security.blade.php
+++
@@ -1,4 +0,0 @@
-
-To prevent users from tampering with Livewire requests between component dehydration and rehydration, Livewire passes a "checksum" along with every request, that ensures all the data, the component name, and id haven't been tampered with.
-
-Outside of that measure, Livewire uses normal Laravel requests, so CSRF, Sanitization, etc... will all be the exact same as a normal AJAX request.
