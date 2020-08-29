0308d70d931372c60146fac299f2084516eae28b
Dan Harrin <git@DanHarrin.com>
2020-08-29 22:21:47 +0100

    Move assets into assets directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,12 +13,12 @@ require('laravel-mix-purgecss');
  |
  */
 
-mix.js('resources/js/main.js', 'public/js')
+mix.js('resources/assets/js/main.js', 'public/js')
     .sourceMaps()
-    .sass('resources/sass/main.scss', 'public/css')
+    .sass('resources/assets/sass/main.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+        postCss: [tailwindcss('./resources/assets/sass/tailwind.config.js')],
     })
     .purgeCss({
         extensions: ['html', 'md', 'js', 'php', 'vue', 'blade'],
