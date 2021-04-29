18494967d22ca8c5461e5f2608af658d71c4d0a1
Saúl Solórzano <hola@saulsolorzano.com>
2021-04-29 16:02:39 -0400

    Config flag is no longer necessary to publish config

--- installation.blade.php
+++ installation.blade.php
@@ -51,7 +51,7 @@ You can publish Livewire's config file with the following artisan command:
 
 @component('components.code', ['lang' => 'bash'])
 @verbatim
-php artisan livewire:publish --config
+php artisan livewire:publish
 @endverbatim
 @endcomponent
 
