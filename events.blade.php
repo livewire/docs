6d061d2f7d49be24d3e3a3dbe702b680fadef81f
Mohammad Morakabati <mhmmdtech@yahoo.com>
2023-06-20 02:18:16 +0330

    Update events.blade.php
    After "name-updated" is called on window, the value of "open" must be changed to "true" in order to display the modal.

--- events.blade.php
+++ events.blade.php
@@ -192,7 +192,7 @@ window.addEventListener('name-updated', event => {
 AlpineJS allows you to easily listen for these window events within your HTML:
 
 @component('components.code', ['lang' => 'blade'])
-<div x-data="{ open: false }" @name-updated.window="open = false">
+<div x-data="{ open: false }" @name-updated.window="open = true">
     <!-- Modal with a Livewire name update form -->
 </div>
 @endcomponent
