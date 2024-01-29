4ef0b3964e432179ec31cab4766db90159e8ea7d
eqxDev <me@eqxdev.com>
2024-01-29 11:22:12 +0000

    Added Cloudflare troubleshooting infomation

--- troubleshooting.blade.php
+++ troubleshooting.blade.php
@@ -8,6 +8,9 @@
 * [Root Element Issues](#root-element-issues)
     * [Symptoms](#root-element-symptoms)
     * [Cures](#root-element-cures)
+* [Cloudflare Compatibility Issues](#cloudflare-compatibility-issues)
+    * [Symptoms](#cloudflare-symptoms)
+    * [Cures](#cloudflare-cures)
 
 
 ## Dom Diffing Issues {#dom-diffing-issues}
@@ -147,3 +150,15 @@ So in our example from above, we have wrapped everything in a `<div>` which gets
 @endcomponent
 
 Another cause can be using __construct() inside the Livewire class or a Trait.
+
+## Cloudflare Compatibility Issues {#cloudflare-compatibility-issues}
+
+When using Livewire in a production environment, you might encounter issues that stem from Cloudflare's Rocket Loader and HTML minification features. These issues can manifest as the page becoming unresponsive or errors indicating that a component is not found.
+
+### Symptoms {#cloudflare-symptoms}
+* Page becomes unresponsive after using `wire:navigate`.
+* Errors like "Component not found: {component_ID}".
+
+### Cures {#cloudflare-cures}
+* **Disable Rocket Loader:** Go to your Cloudflare dashboard, navigate to the "Speed" section, and turn off Rocket Loader. This feature can interfere with the execution order of JavaScript, which is crucial for Livewire's operation.
+* **Disable HTML Minification:** In the Cloudflare dashboard under the "Optimization" section, turn off HTML minification. This will prevent alteration or minification of Livewire's inline scripts, which can lead to operational errors.
